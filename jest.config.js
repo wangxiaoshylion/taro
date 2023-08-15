c0e158de2744a24fda994b14a10e4a84ed01c9cf
wangxiaoshylion <wangxiaoshylion@gmail.com>
2023-08-15 15:16:43 +0800

    fix(utils): jest test配置项变更

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,13 @@
 const defineJestConfig = require('@tarojs/test-utils-react/dist/jest.js').default;
 
 module.exports = defineJestConfig({
-  verbose: true,
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   // transform: {
-  //   "^.+\\.(t|j)sx?$": ['ts-jest'],
+  //   "^.+\\.(ts|tsx)$": "ts-jest"
   // },
   // preset: 'ts-jest',
   moduleNameMapper: {
